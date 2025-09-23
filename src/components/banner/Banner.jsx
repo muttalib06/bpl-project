ff75840ce1439b2d1a742824bddb44e08e2cd1d3
Muttalib <mdabdulmottalib2222@gamil.com>
2025-09-23 10:43:00 +0600

    added navbar and banner section

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import bannerImg from "../../assets/banner-main.png";
+
+const Banner = () => {
+  return (
+    <div className="max-w-[80%] mx-auto bg-[#131313] mt-6 text-white flex justify-center items-center flex-col space-y-5 py-5 rounded-xl bg-gradient-to-tr from-blue-300 via-[#131313] to-pink-300">
+      <img src={bannerImg} alt="" />
+      <h1 className="font-bold text-4xl">Assemble Your Ultimate Dream 11 Cricket Team</h1>
+      <p className="font-medium text-2xl text-gray-500">Beyond Boundaries Beyond Limits</p>
+     <div className="border-[#E7FE29] border rounded-xl py-2 px-2"> <button className="btn border-[#E7FE29] bg-[#E7FE29] text-black  btn-outline">Claim Free Credit</button></div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from "../../assets/logo.png"
+import coin  from "../../assets/coin.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 max-w-[80%] mx-auto mt-5 ">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">
+          <img src={logo} alt="" />
+        </a>
+      </div>
+      <div className="flex items-center justify-between gap-15">
+        <ul className="flex justify-around items-center gap-5 text-[#13131370]font-semibold">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Fixture</a>
+          </li>
+          <li>
+            <a href="">Teams</a>
+          </li>
+          <li>
+            <a href="">Schedules</a>
+          </li>
+        </ul>
+        <div className="flex items-center gap-2 border border-[#13131310] py-1 px-3 rounded-lg shadow">
+          <span className="font-bold">0 coin</span>
+          <img className="w-[20px] h-[20px]" src={coin} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
